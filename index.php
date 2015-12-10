bc45cd8c63de87a6eec7d9845888f71ab39a3674
Mateusz Raniewicz <mateuszraniewicz@gmail.com>
2015-12-09 22:42:01 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<html>
+
+	<body>
+	
+		<h1>Index Page</h1>
+		
+		<h2>Files Contained:</h2>
+		
+		<ul>
+			<li><a href="http://database-access-scripts.azurewebsites.net/get_agency.php">get_agency.php</a></li>
+			<li><a href="http://database-access-scripts.azurewebsites.net/get_routes.php">get_routes.php</a></li>
+			<li><a href="http://database-access-scripts.azurewebsites.net/get_stops.php">get_stops.php</a></li>
+			<li><a href="http://database-access-scripts.azurewebsites.net/get_timetable.php">get_timetable.php</a></li>
+		</ul>
+	
+	</body>
+
+</html>
