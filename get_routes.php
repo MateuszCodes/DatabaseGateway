58984629dd1c4ce70ac6d019a64ffcaf47d1ad5c
Mateusz Raniewicz <mateuszraniewicz@gmail.com>
2015-12-09 22:44:35 -0500

    Create get_routes.php

---
+++ get_routes.php
@@ -0,0 +1,54 @@
+<?php
+
+	$con = mysqli_connect("hostName", "user", "pass", "DBName");
+	
+	if (mysqli_connect_errno($con)){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+	$agencyName = $_GET['agencyName'];
+	
+	$result = mysqli_query($con, "SELECT agencyID FROM agencies WHERE agencyName = '$agencyName'");
+
+	$row = mysqli_fetch_array($result);
+	$agencyID = $row[0];
+	
+	$result = mysqli_query($con, "SELECT routeShortName, direction FROM routes WHERE agencyID = '$agencyID' ORDER BY routeShortName ASC");
+
+	$row = mysqli_fetch_array($result);
+	$data = $row[0];
+	
+	if($data){
+		echo $data;
+	}
+	
+	mysqli_close($con);
+?>
+
+<html>
+
+	<body>
+	
+		<h2>Tables Used</h2>
+		
+		<ul>
+			<li>Agencies</li>
+			<li>Routes</li>
+		</ul>
+		
+		<h2>Arguments Required</h2>
+		
+		<ul>
+			<li>agencyName</li>
+		</ul>
+		
+		<h2>Attributes Returned</h2>
+		
+		<ul>
+			<li>routeShortName</li>
+			<li>direction</li>
+		</ul>
+	
+	</body>
+
+</html>
